02484a540ebfc48697c842b773ff59fda354630d
André Luiz dos Santos <andreluiz1793@gmail.com>
2021-10-23 00:13:50 -0300

    Add files via upload

---
+++ effecti-licitacoesAPI/src/main/java/com/effecti/gerenciador/EffectiLicitacoesGerenciador.java
@@ -0,0 +1,17 @@
+package com.effecti.gerenciador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@Configuration
+public class EffectiLicitacoesGerenciador {
+
+	public static void main(String[] args) {
+			SpringApplication.run(EffectiLicitacoesGerenciador.class, args);
+
+	}
+
+}
---
+++ effecti-licitacoesAPI/src/main/java/com/effecti/gerenciador/services/LicitacoesServices.java
@@ -0,0 +1,40 @@
+package com.effecti.gerenciador.services;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.effecti.gerenciador.entities.LicitacaoEntity;
+import com.effecti.gerenciador.repository.LicitacoesRepository;
+import com.effecti.gerenciador.webScrapping.WebScrapping;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Service
+public class LicitacoesServices {
+	
+	@Autowired
+	LicitacoesRepository licitacoesRepository;
+
+	public static void converterToJson(LicitacaoEntity licitacaoEntity) {
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			String json = mapper.writeValueAsString(licitacaoEntity);
+			StringBuilder string = new StringBuilder("Objeto em JSON: ");
+			System.out.println(string.append(json));
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void salvaLicitacoes() throws IOException {//Esse método executa o WebScrapping e então pega os dados que chegaram e vai salvando eles um por um em objetos no repositório
+		List<LicitacaoEntity> listaLicitacoes = WebScrapping.extrairDados();
+		for (LicitacaoEntity licitacao:listaLicitacoes) {//Percorre a lista toda de Json retornado pelo WebScrapping um por um, é basicamente um forEach
+			System.out.println("Salvando licitação: " + licitacao.getLicitacao());
+			licitacoesRepository.save(licitacao);
+		}
+		System.out.println("Acabou");
+	}
+}
